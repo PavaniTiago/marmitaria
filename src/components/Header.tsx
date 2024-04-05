2573657ead3ec77275d90132c253e4ab8cd06463
PavaniTiago <tiagopavanisalto@hotmail.com>
2024-04-05 13:08:14 -0300

    fix: fixed sidebar-btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image";
 import logo from "../../public/logo.png"
 import Link from "next/link";
@@ -16,9 +18,9 @@ import {
   
 export function Header(){
     return (
-        <header className="flex absolute w-full items-center backdrop-blur-xl justify-between px-12 lg:px-24 h-40">
-            <div className="flex items-center justify-center w-28 h-28 rounded-full bg-white">
-                <Image src={logo} alt="marmitaria logo" height={0} width={0} className="object-cover w-36 h-36 mt-[2.5px]"></Image>
+        <header className="flex absolute w-full items-center backdrop-blur-xl justify-between px-12 lg:px-24 h-28 lg:h-40">
+            <div className="flex items-center justify-center w-24 h-24 lg:w-28 lg:h-28 rounded-full bg-white">
+                <Image src={logo} alt="marmitaria logo" height={0} width={0} className="object-cover w-32 h-32 lg:w-36 lg:h-36 mt-[2.5px]"></Image>
             </div>
             <div className="hidden lg:block">
                 <div className="flex  gap-8 text-white">
@@ -36,11 +38,11 @@ export function Header(){
                     <SheetContent>
                     <SheetHeader className="lg:ml-4 mt-6 lg:pl-6 pt-6 space-y-0">
                         <SheetTitle className="text-lg text-foreground/60 pt-3">Acesse nosso menu</SheetTitle>
-                        <SheetDescription className="text-md text-black font-medium hover:bg-gray-200 transition-colors cursor-pointer"><Link href="#menu">Menu</Link></SheetDescription>
-                        <SheetTitle className="text-lg text-foreground/60 pt-3">Contato</SheetTitle>
-                        <SheetDescription className="text-md text-black font-medium hover:bg-gray-200 transition-colors cursor-pointer"><Link href="https://linktr.ee/saboraoponto_">Fale conosco</Link></SheetDescription>
+                        <SheetTrigger className="text-md text-black font-medium hover:bg-gray-200 transition-colors cursor-pointer"><Link href="#menu">Menu</Link></SheetTrigger>
                         <SheetTitle className="text-lg text-foreground/60 pt-3">Sobre</SheetTitle>
-                        <SheetDescription className="text-md text-black font-medium hover:bg-gray-200 transition-colors cursor-pointer"><Link href="#sobre">Nossa nutricionista</Link></SheetDescription>
+                        <SheetTrigger className="text-md text-black font-medium hover:bg-gray-200 transition-colors cursor-pointer"><Link href="#sobre">Nossa nutricionista</Link></SheetTrigger>
+                        <SheetTitle className="text-lg text-foreground/60 pt-3">Contato</SheetTitle>
+                        <SheetTrigger className="text-md text-black font-medium hover:bg-gray-200 transition-colors cursor-pointer"><Link href="https://linktr.ee/saboraoponto_">Fale conosco</Link></SheetTrigger>
                     </SheetHeader>
                     <SheetFooter>
                         <Link target="_blank" className="absolute bottom-6 font-semibold text-md" href="https://whatsmenu.com.br/saboraoponto"><Button className="w-64 py-6">Peça agora</Button></Link>
